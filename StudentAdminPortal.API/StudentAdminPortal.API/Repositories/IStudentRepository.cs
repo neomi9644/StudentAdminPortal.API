1fcda859c49f2b0308255de85c5295f677a58740
neomi9644@ <neomi9644@gmail.com>
2023-02-16 10:42:08 +0200

    End of Section 7

--- StudentAdminPortal.API/StudentAdminPortal.API/Repositories/IStudentRepository.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/IStudentRepository.cs
@@ -14,7 +14,7 @@ namespace StudentAdminPortal.API.Repositories
         Task<List<Gender>> GetGenderAsync();
         Task<bool> Exists(Guid studentId);
         Task<Student> UpdateStudent(Guid studentId, Student request);
-
         Task<Student> DeleteStudent(Guid studentId);
+        Task<Student> AddStudent(Student request);
     }
 }
