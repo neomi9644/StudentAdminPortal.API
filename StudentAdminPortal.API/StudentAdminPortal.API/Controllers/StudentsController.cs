b808b5d6aeee2b034620150d2eb9f7d0c723e5c0
neomi9644@ <neomi9644@gmail.com>
2023-02-19 12:32:07 +0200

    End of section 9

--- StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/Controllers/StudentsController.cs
@@ -91,19 +91,33 @@ namespace StudentAdminPortal.API.Controllers
         [Route("[controller]/{studentId:guid}/upload-image")]
         public async Task<IActionResult> UploadImage([FromRoute] Guid studentId, IFormFile profileImage)
         {
-            if (await studentRepository.Exists(studentId))
+            var validEtensions = new List<string>()
             {
-                var fileName = Guid.NewGuid() + Path.GetExtension(profileImage.FileName);
-                var fileImagePath = await imageRepository.Upload(profileImage, fileName);
-                if (await studentRepository.UpdateProfileImage(studentId, fileImagePath))
+                "jpeg",
+                "png",
+                "gif"
+            };
+            if(profileImage != null && profileImage.Length>0 )
+            {
+                var extention = Path.GetExtension(profileImage.FileName);
+                if (validEtensions.Contains(extention))
                 {
-                    return Ok();
-                }
 
-                return StatusCode(StatusCodes.Status500InternalServerError, "Error uploading image");
-                
-                
+                    if (await studentRepository.Exists(studentId))
+                    {
+                        var fileName = Guid.NewGuid() + Path.GetExtension(profileImage.FileName);
+                        var fileImagePath = await imageRepository.Upload(profileImage, fileName);
+                        if (await studentRepository.UpdateProfileImage(studentId, fileImagePath))
+                        {
+                            return Ok();
+                        }
+
+                        return StatusCode(StatusCodes.Status500InternalServerError, "Error uploading image");
+                    }
+                }
+                return BadRequest("This is not async valid Image format");
             }
+           
             return NotFound();
         }
     }
